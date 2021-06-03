9b32135ce93c254bd919ebb8512d419a751d132d
shraddhapai <shraddha.pai@utoronto.ca>
2021-06-03 16:05:27 -0400

    update

--- book/02-BuildPredictor.R
+++ book/02-BuildPredictor.R
@@ -1,61 +1,66 @@
-## ----echo=TRUE, fig.cap="Lab 2: Predictor design", out.width="50%"----
+## ---- class.source="codeblock",echo=TRUE, fig.cap="Lab 1 design: We will integrate four layers of genomic data. Each layer will be converted into a single patient similarity network using Pearson correlation for pairwise similarity. ",echo=FALSE----
 knitr::include_graphics("images/Lab1_design.jpg")
 
 
-## ----eval=TRUE---------------------------------------------------
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 suppressMessages(library(curatedTCGAData))
 
 
-## ----eval=TRUE---------------------------------------------------
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 curatedTCGAData(diseaseCode="BRCA", assays="*",dry.run=TRUE)
 
 
-## ----eval=TRUE---------------------------------------------------
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 brca <- suppressMessages(curatedTCGAData("BRCA",
                                          c("mRNAArray","Methylation_methyl27", 
 										 "RPPAArray","miRNASeqGene"),
                                          dry.run=FALSE))
 
 
-## ----eval=TRUE---------------------------------------------------
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 brca
 
 
-## ----eval=TRUE---------------------------------------------------
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 summary(assays(brca))
 
 
-## ----eval=TRUE---------------------------------------------------
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 names(assays(brca))
 
 
-## ----eval=TRUE---------------------------------------------------
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 mir <- assays(brca)[["BRCA_miRNASeqGene-20160128"]]
 head(mir[,1:5])
 
 
-## ----eval=TRUE---------------------------------------------------
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 pheno <- colData(brca)
 colnames(pheno)[1:20]
 head(pheno[,1:5])
 
 
-## ----eval=TRUE---------------------------------------------------
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 source("prepare_data.R")
 brca <- prepareDataForCBW(brca)
 
 
-## ----eval=TRUE---------------------------------------------------
+## 
+## netDx requires `ID` and `STATUS` columns in `colData(...)`. Be sure to define these in the code you use to prepare the data. Otherwise the function to build the predictor will return an error.
+
+## 
+
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 pheno <- colData(brca)
 head(pheno[,c("ID","STATUS")])
 table(pheno$STATUS,useNA="always")  # good practice: useNA="always" shows missing values
 
 
-## ----eval=TRUE---------------------------------------------------
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 suppressWarnings(suppressMessages(require(netDx)))
 
 
-## ----eval=TRUE---------------------------------------------------
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 expr <- assays(brca)
 groupList <- list()
 for (k in 1:length(expr)) {	# loop over all layers
@@ -69,17 +74,17 @@ for (k in 1:length(expr)) {	# loop over all layers
 }
 
 
-## ----eval=TRUE---------------------------------------------------
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 summary(groupList)
 
 
-## ----eval=TRUE---------------------------------------------------
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 names(groupList[["BRCA_mRNAArray-20160128"]])
 length(groupList[["BRCA_mRNAArray-20160128"]][[1]])
 head(groupList[["BRCA_mRNAArray-20160128"]][[1]])
 
 
-## ---- eval=TRUE--------------------------------------------------
+## ---- class.source="codeblock", eval=TRUE----------------------------------------------------------------
 makeNets <- function(dataList, groupList, netDir,...) {
 	netList <- c() # initialize before is.null() check
 	
@@ -108,7 +113,13 @@ makeNets <- function(dataList, groupList, netDir,...) {
 
 
 
-## ----eval=TRUE---------------------------------------------------
+## While netDx provides a high degree of flexibility in achieving your design of choice, it is up to the user to ensure that the design, i.e. choice of similarity metrics and variable groupings, is appropriate for your application. Domain knowledge is almost likely required for good design.
+
+
+## These are unrealistically low values set so the example will run fast. In practice a good starting point is `featScoreMax=10`, `featSelCutoff=9` and `numSplits=10L`, but these parameters depend on the sample sizes in the dataset and heterogeneity of the samples.
+
+
+## ----lab1-buildpredictor ,class.source="codeblock",eval=TRUE---------------------------------------------
 set.seed(42) # make results reproducible
 outDir <- paste(tempdir(),randAlphanumString(),
 	"pred_output",sep=getFileSep())
@@ -134,41 +145,41 @@ t1 <- Sys.time()
 print(t1-t0)
 
 
-## ----eval=TRUE---------------------------------------------------
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 summary(model)
 summary(model$Split1)
 
 
-## ----eval=TRUE---------------------------------------------------
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 outFile <- sprintf("%s/CBW_Lab1_full.rda",tempdir())
 download.file("https://github.com/RealPaiLab/CBW_CAN_DataIntegration_2021/raw/master/supporting_files/Lab1_files/Lab1_10splits.rda",
 	destfile=outFile)
 lnames <- load(outFile)
 
 
-## ----eval=TRUE---------------------------------------------------
+## ----lab1-getresults,class.source="codeblock",eval=TRUE--------------------------------------------------
 source("helper.R")
-results <- getResults(brca,model=model_full,featureSelCutoff=9L,
+results <- getResults(brca,model_full,featureSelCutoff=9L,
 	featureSelPct=0.9)
 
 
-## ----eval=TRUE---------------------------------------------------
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 summary(results)
 
 
-## ----eval=TRUE---------------------------------------------------
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 results$performance
 
 
-## ---- eval=TRUE--------------------------------------------------
+## ---- class.source="codeblock", eval=TRUE----------------------------------------------------------------
 results$featureScores
 
 
-## ----eval=TRUE---------------------------------------------------
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 results$selectedFeatures
 
 
-## ----fig.width=8,fig.height=8, eval=TRUE-------------------------
+## ---- class.source="codeblock",fig.width=8,fig.height=8, eval=TRUE---------------------------------------
 psnFile <- sprintf("%s/psn.rda",tempdir())
 download.file("https://github.com/RealPaiLab/CBW_CAN_DataIntegration_2021/raw/master/supporting_files/Lab1_files/Lab1_PSN.rda",
 	destfile=outFile)
@@ -181,6 +192,10 @@ tsne <- tSNEPlotter(
 	)
 
 
-## ----------------------------------------------------------------
+##  We end this tutorial with a call to `sessionInfo()` which prints the complete environment information for your R session. This is standard output that should be reported to R package managers when you write in with a question, particularly if reporting an error or bug. Sometimes a particular underlying dependency package may be the cause of an error, or you may need to upgrade to a newer version of the package. By including this info in an email, you will allow the other person to better solve your issue.
+
+## 
+
+## --------------------------------------------------------------------------------------------------------
 sessionInfo()
 
--- book/03-PathwayFeatures.R
+++ book/03-PathwayFeatures.R
@@ -1,8 +1,20 @@
-## ----echo=FALSE, fig.cap="Lab 2: Predictor design"---------------------------------------------------------------------------------------------------
+##  1. In practice I recommend running a predictor design with 2-3 different sets of pathway definitions, and comparing the predictive pathway themes. For instance, it could be useful to compare results from using all curated pathways, to just domain-specific ones.
+
+##  Feature design is something of an art, and the choice of pathways depends on what your goals with building the predictor are. Are you looking to prioritize a known set of biological processes or interested in general discovery? These are tradeoffs.
+
+##  At the very least I would recommend running with all curated pathways as a baseline, because you may generate novel hypotheses.
+
+## 
+##  2. Note that while in this design we group gene expression measures into pathways, the same design can be used to group other types of data based on prior knowledge. For instance, measures from imaging data could be grouped by prior knowledge of correlated networks of regions of interest subserving specific functions.
+
+## 
+
+
+## ---- class.source="codeblock",echo=FALSE, fig.cap="Lab 2 design: We will integrate clinical and gene expression data. Each layer will be converted into a single patient similarity network using Pearson correlation for pairwise similarity.", echo=FALSE----
 knitr::include_graphics("images/Lab2_design.jpg")
 
 
-## ----load-data,eval=TRUE-----------------------------------------------------------------------------------------------------------------------------
+## ---- class.source="codeblock", class.source="codeblock",eval=TRUE---------------------------------------
 suppressMessages(library(curatedTCGAData))
 brca <- suppressMessages(
    curatedTCGAData(
@@ -11,69 +23,79 @@ brca <- suppressMessages(
 	)
 
 
-## ----eval=TRUE---------------------------------------------------------------------------------------------------------------------------------------
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 brca
 
 
-## ----eval=TRUE---------------------------------------------------------------------------------------------------------------------------------------
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 source("prepare_data.R")
 brca <- prepareDataForCBW(brca, setBinary=TRUE)
 
 
-## ----eval=TRUE---------------------------------------------------------------------------------------------------------------------------------------
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 suppressWarnings(suppressMessages(require(netDx)))
 groupList <- list()
 
 
-## ----eval=TRUE---------------------------------------------------------------------------------------------------------------------------------------
+## This design changes model-building time to several hours, so avoid large gene sets (e.g. the full set of ~44,000 Gene Ontology terms, or even ~29,000 GO Biological Process terms). A reasonable start is a compilation of pathways from all curated pathway databases, as in below.
+
+## Whichever list you use can be pruned by constraining the min/max number of genes in a set, but the size is something to keep in mind.
+
+
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 x <- fetchPathwayDefinitions("March",2021)
 x
 
 
-## ----echo=FALSE, fig.cap="Lab 2: Predictor design"---------------------------------------------------------------------------------------------------
+## ---- class.source="codeblock",echo=FALSE, fig.cap="Lab 2: Example of GMT file format."------------------
 knitr::include_graphics("images/GMT_screenshot.png")
 
 
-## ----eval=TRUE---------------------------------------------------------------------------------------------------------------------------------------
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 pathList <- readPathways(fetchPathwayDefinitions("March",2021))
 head(pathList)
 
 
-## ----eval=TRUE---------------------------------------------------------------------------------------------------------------------------------------
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 gmtFile <- sprintf("%s2/cancer_pathways.gmt",tempdir())
-if (!file.exists(sprintf("%s2",tempdir()))) dir.create(sprintf("%s2",tempdir()))
+if (!file.exists(sprintf("%s2",tempdir()))) {
+	dir.create(sprintf("%s2",tempdir()))
+}
 download.file("https://raw.githubusercontent.com/RealPaiLab/CBW_CAN_DataIntegration_2021/master/supporting_files/c6.all.v7.4.symbols.gmt",gmtFile)
 x <- readPathways(gmtFile)
 x[1:3]
 
 
-## ----eval=TRUE---------------------------------------------------------------------------------------------------------------------------------------
+## The pathway definition file should use the same identifier type as your patient data. For instance, if the genes in your transcriptomic data are represented using [HGNC symbols](https://www.genenames.org/tools/search/), then your pathway definition file must also use HGNC symbols (e.g. *ID2S*), and not a different type of identifier, such as Ensembl IDs (which look like this: *ENSG00000010404*).
+
+
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 groupList[["BRCA_mRNAArray-20160128"]] <- pathList 
 
 
-## ----eval=TRUE---------------------------------------------------------------------------------------------------------------------------------------
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 pheno <- colData(brca)
 head(pheno[,c("patient.age_at_initial_pathologic_diagnosis","STAGE")])
 
 
-## ----eval=TRUE---------------------------------------------------------------------------------------------------------------------------------------
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 groupList[["clinical"]] <- list(
       age="patient.age_at_initial_pathologic_diagnosis",
 	   stage="STAGE"
 )
 
 
-## ----eval=FALSE--------------------------------------------------------------------------------------------------------------------------------------
+## ---- class.source="codeblock",eval=FALSE----------------------------------------------------------------
 ## makePSN_NamedMatrix(..., writeProfiles=TRUE,...)`
 
 
-## ----eval=FALSE--------------------------------------------------------------------------------------------------------------------------------------
+## ---- class.source="codeblock",eval=FALSE----------------------------------------------------------------
 ## makePSN_NamedMatrix(,...,
 ## 	simMetric="custom", customFunc=normDiff,
 ## 	writeProfiles=FALSE)
 
 
-## ----eval=TRUE---------------------------------------------------------------------------------------------------------------------------------------
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 makeNets <- function(dataList, groupList, netDir,...) {
 	netList <- c() 
 
@@ -108,7 +130,7 @@ makeNets <- function(dataList, groupList, netDir,...) {
 }
 
 
-## ----eval=TRUE---------------------------------------------------------------------------------------------------------------------------------------
+## ----lab2-buildpredictor,class.source="codeblock",eval=TRUE----------------------------------------------
 t0 <- Sys.time()
 set.seed(42) # make results reproducible
 outDir <- paste(sprintf("%s2",tempdir()),"pred_output",sep=getFileSep()) # use absolute path
@@ -130,29 +152,46 @@ t1 <- Sys.time()
 print(t1-t0) # time taken
 
 
-## ----eval=TRUE---------------------------------------------------------------------------------------------------------------------------------------
+## ----lab2-getresults, class.source="codeblock", eval=TRUE------------------------------------------------
 outFile <- sprintf("%s2/CBW_Lab2_full.rda",tempdir())
 download.file("https://github.com/RealPaiLab/CBW_CAN_DataIntegration_2021/raw/master/supporting_files/Lab2_files/brca_binary_pathways.rda",
 	destfile=outFile)
 lnames <- load(outFile)
 
 
-## ----eval=TRUE---------------------------------------------------------------------------------------------------------------------------------------
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 source("helper.R")
 results <- getResults(brca,model_full,
 	featureSelCutoff=9L,
 	featureSelPct=0.9)
 
 
-## ----eval=TRUE---------------------------------------------------------------------------------------------------------------------------------------
+## ----class.source="codeblock",eval=TRUE------------------------------------------------------------------
+perf <- results$performance
+round(mean(perf$splitAUROC),2)*100
+round(mean(perf$splitAUPR),2)*100
+round(mean(perf$splitAccuracy),2)*100
+
+
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 confMat <- confusionMatrix(model_full)
 
 
-## ----eval=TRUE---------------------------------------------------------------------------------------------------------------------------------------
+## Particularly when there is class imbalance, i.e. one class has several-fold the number of samples than the other, predictors can achieve a baseline high accuracy by "cheating" and simply predicting all samples as being of the dominating label. e.g. if you have 99:1 class imbalance of two classes A and B, the predictor can achieve 99% accuracy simply by calling all samples of type "A"!
+
+## 
+## In practice, class imbalance should be handled using suitable performance evaluation metrics and sampling proportionally for training/test sets.
+
+
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
+table(colData(brca)$STATUS,useNA="always")
+
+
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 summary(confMat)
 
 
-## ----eval=TRUE---------------------------------------------------------------------------------------------------------------------------------------
+## ----lab2-makeemapinput ,eval=TRUE-----------------------------------------------------------------------
 emap <- makeInputForEnrichmentMap (
 	model=model_full,
 	results=results,
@@ -164,19 +203,22 @@ emap <- makeInputForEnrichmentMap (
 )
 
 
-## ----eval=TRUE---------------------------------------------------------------------------------------------------------------------------------------
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 emap
 
 
-## ----eval=TRUE---------------------------------------------------------------------------------------------------------------------------------------
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 ###plotEmap(gmtFiles[[1]],nodeAttrFiles[[1]],
 ###         groupClusters=TRUE, hideNodeLabels=TRUE)
 
 
-## ----eval=TRUE---------------------------------------------------------------------------------------------------------------------------------------
+## ----echo=FALSE, out.width="140%",  fig.cap="Lab 2: EnrichmentMap for top-scoring pathways predictive of Luminal A status. Nodes show pathways scoring 7+ out of 10 in over 70% of train/test splits. Edges connect pathways with shared genes. Yellow bubbles show pathway clusters, labelled by a AutoAnnotate. Node fills indicate top score in 70%+ of the splits. Speech bubbles show pathways in example clusters."----
 knitr::include_graphics("images/Lab2_EMap.jpg")
 
 
-## ----eval=TRUE---------------------------------------------------------------------------------------------------------------------------------------
+##  (1) In practice, it can take a good portion of an hour to adjust the layout of the EnrichmentMap, and often longer to explore the contents. The automatically-generated pathway theme labels are often revised upon looking closer inspection of the pathways within.
+
+
+## ---- class.source="codeblock",eval=TRUE-----------------------------------------------------------------
 sessionInfo()
 
